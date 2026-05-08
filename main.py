5ecd672e83adec89bcc18585b30568570a926004
Nimalan Mani <mnimalanmani@gmail.com>
2026-05-08 13:59:17 +0530

    Project Files

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 import os
-import threading
-import webbrowser
 
 import uvicorn
 
 
-def open_browser():
-    webbrowser.open(
-        "http://127.0.0.1:8000/docs"
-    )
-
-
 if __name__ == "__main__":
-    threading.Timer(
-        1.5,
-        open_browser
-    ).start()
-
     uvicorn.run(
         "api.app:app",
         host="0.0.0.0",
@@ -25,6 +12,5 @@ if __name__ == "__main__":
                 "PORT",
                 8000
             )
-        ),
-        reload=True
+        )
     )
